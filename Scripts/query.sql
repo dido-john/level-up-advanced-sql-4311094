ce5b592c883d910becc3eae17e9551f544e15041
dido-john <131214672+dido-john@users.noreply.github.com>
2024-06-18 04:54:49 +0000

    june18

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -2,8 +2,15 @@ SELECT *
 FROM employee;
 
 
-SELECT *
-FROM sales;
+SELECT soldDate
+FROM sales
+LIMIT 100 ;
+
+SELECT c.firstName, c.lastName,c.email, s.salesAmount, s.soldDate
+FROM customer c
+FULL OUTER JOIN sales s
+ON c.customerId = s.customerId
+--WHERE c.customerId any;
 
 GO
 
@@ -21,11 +28,36 @@ SELECT sql
 FROM sqlite_schema 
 WHERE name = 'employee';
 
-SELECT  A.firstName , A.lastName, B.salesAmount as total
+SELECT  A.firstName , A.lastName, A.title, B.salesAmount as total
 FROM employee A 
 left join sales B
 ON ( A.employeeId = B.employeeId) 
-WHERE B.salesAmount is NULL or B.salesAmount=0
+WHERE B.salesAmount is NULL  AND A.title = 'Sales Person' 
+;
 
+SELECT  A.employeeId, A.firstName , A.lastName,count(*) as NumofCarsSold
+FROM employee A 
+inner join sales B
+ON ( A.employeeId = B.employeeId) 
+WHERE  A.title = 'Sales Person'
+GROUP BY  A.employeeId, A.firstName , A.lastName
+ORDER BY NumofCarsSold DESC
+;
 
+SELECT  A.employeeId, A.firstName , A.lastName,  min(B.salesAmount) as cheap , max(B.salesAmount) as expensive
+FROM employee A 
+INNER join sales B
+ON ( A.employeeId = B.employeeId) 
+--WHERE B.soldDate >= date('now', 'start of year')
+GROUP BY  A.employeeId, A.firstName , A.lastName 
+;
+
+SELECT  A.employeeId, A.firstName , A.lastName,count(*) as NumofCarsSold
+FROM employee A 
+inner join sales B
+ON ( A.employeeId = B.employeeId) 
+--WHERE B.soldDate >= date('now', 'start of year')
+GROUP BY  A.employeeId, A.firstName , A.lastName
+HAVING NumofCarsSold > 5
+--ORDER BY NumofCarsSold DESC
 ;
